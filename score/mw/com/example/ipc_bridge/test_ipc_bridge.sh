debcf7b9513353aad61d9642c340eb6d95ed1e01
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2025-08-11 10:47:55 +0000

    adapt test execution to new interface

--- score/mw/com/example/ipc_bridge/test_ipc_bridge.sh
+++ score/mw/com/example/ipc_bridge/test_ipc_bridge.sh
@@ -1,47 +1,53 @@
 #!/bin/bash
 set -eux
 
-CPP_EXAMPLE_CMD="score/mw/com/example/ipc_bridge/ipc_bridge_cpp -s score/mw/com/example/ipc_bridge/etc/mw_com_config.json -n 20 -t 1000"
-RUST_EXAMPLE_CMD="score/mw/com/example/ipc_bridge/ipc_bridge_rs -s score/mw/com/example/ipc_bridge/etc/mw_com_config.json"
+# this cannot work if both sender and receiver expect 20 samples
+# the sender has to send infinite samples and the receiver has to terminate after having received some
+CPP_EXAMPLE_CMD="score/mw/com/example/ipc_bridge/ipc_bridge_cpp -s score/mw/com/example/ipc_bridge/etc/mw_com_config.json --cycle-time 1000"
+RUST_EXAMPLE_CMD="score/mw/com/example/ipc_bridge/ipc_bridge_rs -s score/mw/com/example/ipc_bridge/etc/mw_com_config.json --cycle-time 1000"
 
 function run_receiver_sender() {
     EXAMPLE_CMD_RECV="$1"
     EXAMPLE_CMD_SEND="$2"
-    RECEIVER_RETURN_CODE=$3
-    KILL_RECEIVER="$4"
 
     tempdir=$(mktemp -d /tmp/ipc_bridge.XXXXXX)
 
+    # Ensure we start with a clean state
+    rm -rf /tmp/mw_com_lola
+    rm -rf /dev/shm/lola-*
+
     # Run examples
-    $EXAMPLE_CMD_RECV -m recv > "$tempdir/recv.log" 2>&1 &
-    receiver_pid=$!
-    $EXAMPLE_CMD_SEND -m send > "$tempdir/send.log" 2>&1
+    $EXAMPLE_CMD_SEND --num-cycles 0 --mode send > "$tempdir/send.log" 2>&1 &
+    sender_pid=$!
+    $EXAMPLE_CMD_RECV --num-cycles 2 --mode recv > "$tempdir/recv.log" 2>&1
 
     # Check if the receiver received the message
     grep -q "Subscrib" "$tempdir/recv.log"
     grep -q "Received sample" "$tempdir/recv.log"
     rm -rf "$tempdir"
 
-    # Kill receiver and check its return code
-    if [[ "$KILL_RECEIVER" == "true" ]]; then
-        kill $receiver_pid
-    fi
+    # Kill sender and check its return code
+    kill $sender_pid
 
     set +e
-    wait $receiver_pid
-    receiver_return_code=$?
+    wait $sender_pid
+    sender_return_code=$?
     set -e
-    [[ "$RECEIVER_RETURN_CODE" == "$receiver_return_code" ]]
+    [[ "143" == "$sender_return_code" ]]
+
+    # Cleanup due to SIGINT
+    rm -rf /tmp/mw_com_lola
+    rm -rf /dev/shm/lola-*
 }
 
 echo -e "\n\n\nRunning Rust receiver and Rust sender"
-run_receiver_sender "$RUST_EXAMPLE_CMD" "$RUST_EXAMPLE_CMD" 143 true
+run_receiver_sender "$RUST_EXAMPLE_CMD" "$RUST_EXAMPLE_CMD"
 
 echo -e "\n\n\nRunning C++ receiver and C++ sender"
-run_receiver_sender "$CPP_EXAMPLE_CMD" "$CPP_EXAMPLE_CMD" 0 false
+run_receiver_sender "$CPP_EXAMPLE_CMD" "$CPP_EXAMPLE_CMD"
 
 echo -e "\n\n\nRunning Rust receiver and C++ sender"
-run_receiver_sender "$RUST_EXAMPLE_CMD" "$CPP_EXAMPLE_CMD" 143 true
+run_receiver_sender "$RUST_EXAMPLE_CMD" "$CPP_EXAMPLE_CMD"
 
-echo -e "\n\n\nRunning C++ receiver and Rust sender"
-run_receiver_sender "$CPP_EXAMPLE_CMD" "$RUST_EXAMPLE_CMD" 1 false
+# echo -e "\n\n\nRunning C++ receiver and Rust sender"
+# run_receiver_sender "$CPP_EXAMPLE_CMD" "$RUST_EXAMPLE_CMD"
