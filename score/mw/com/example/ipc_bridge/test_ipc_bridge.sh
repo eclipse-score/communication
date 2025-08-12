36dbe8b523019ff5e460d153dc3cb877e15bcf0a
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2025-08-12 21:13:24 +0000

    finally stable service discovery
    lola runs with the implicit assumption that each application instance is run with a different user id. Otherwise data will be overwritten by later started applications and service discovery will be unreliable. Thus the test applications need to be run as different users.
    
    Should be related to https://github.com/eclipse-score/communication/issues/29

--- score/mw/com/example/ipc_bridge/test_ipc_bridge.sh
+++ score/mw/com/example/ipc_bridge/test_ipc_bridge.sh
@@ -1,10 +1,96 @@
 #!/bin/bash
-set -eux
+set -euxo pipefail
+
+MANIFEST_LOCATION="/tmp/mw_com_lola_service_manifest.json"
 
 # this cannot work if both sender and receiver expect 20 samples
 # the sender has to send infinite samples and the receiver has to terminate after having received some
-CPP_EXAMPLE_CMD="score/mw/com/example/ipc_bridge/ipc_bridge_cpp -s score/mw/com/example/ipc_bridge/etc/mw_com_config.json --cycle-time 1000"
-RUST_EXAMPLE_CMD="score/mw/com/example/ipc_bridge/ipc_bridge_rs -s score/mw/com/example/ipc_bridge/etc/mw_com_config.json --cycle-time 1000"
+CPP_EXAMPLE_CMD="score/mw/com/example/ipc_bridge/ipc_bridge_cpp -s $MANIFEST_LOCATION --cycle-time 1000"
+RUST_EXAMPLE_CMD="score/mw/com/example/ipc_bridge/ipc_bridge_rs -s $MANIFEST_LOCATION --cycle-time 1000"
+
+function cleanup_lola() {
+    # Ensure tests are run in a clean state
+    rm -rf /dev/shm/lola-*6432*
+    rm -rf /tmp/mw_com_lola/*/*6432*
+}
+
+function create_service_manifest() {
+    local file="$1"
+    local provider_user_id=$2
+    local consumer_user_id=$3
+
+    cat <<EOF > "$file"
+{
+  "serviceTypes": [
+    {
+      "serviceTypeName": "/bmw/adp/MapApiLanesStamped",
+      "version": {
+        "major": 1,
+        "minor": 0
+      },
+      "bindings": [
+        {
+          "binding": "SHM",
+          "serviceId": 6432,
+          "events": [
+            {
+              "eventName": "map_api_lanes_stamped",
+              "eventId": 1
+            },
+            {
+              "eventName": "dummy_data_stamped",
+              "eventId": 2
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "serviceInstances": [
+    {
+      "instanceSpecifier": "xpad/cp60/MapApiLanesStamped",
+      "serviceTypeName": "/bmw/adp/MapApiLanesStamped",
+      "version": {
+        "major": 1,
+        "minor": 0
+      },
+      "instances": [
+        {
+          "instanceId": 1,
+          "allowedConsumer": {
+            "QM": [
+              $consumer_user_id
+            ]
+          },
+          "allowedProvider": {
+            "QM": [
+              $provider_user_id
+            ]
+          },
+          "asil-level": "QM",
+          "binding": "SHM",
+          "events": [
+            {
+              "eventName": "map_api_lanes_stamped",
+              "numberOfSampleSlots": 10,
+              "maxSubscribers": 3
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+EOF
+}
+
+function setup() {
+    sudo useradd -g vscode mw_com_lola || true
+    # run as mw_com_lola user via sudo without password
+    echo "vscode ALL=(mw_com_lola) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/vscode_mw_com_lola
+    create_service_manifest "$MANIFEST_LOCATION" $(id -u) $(id -u mw_com_lola)
+    trap "sudo deluser mw_com_lola && sudo rm -f /etc/sudoers.d/vscode_mw_com_lola $MANIFEST_LOCATION" EXIT
+}
 
 function run_receiver_sender() {
     EXAMPLE_CMD_RECV="$1"
@@ -13,13 +99,12 @@ function run_receiver_sender() {
     tempdir=$(mktemp -d /tmp/ipc_bridge.XXXXXX)
 
     # Ensure we start with a clean state
-    rm -rf /tmp/mw_com_lola
-    rm -rf /dev/shm/lola-*
+    cleanup_lola
 
     # Run examples
     $EXAMPLE_CMD_SEND --num-cycles 0 --mode send > "$tempdir/send.log" 2>&1 &
     sender_pid=$!
-    $EXAMPLE_CMD_RECV --num-cycles 2 --mode recv > "$tempdir/recv.log" 2>&1
+    sudo -u mw_com_lola $EXAMPLE_CMD_RECV --num-cycles 2 --mode recv > "$tempdir/recv.log" 2>&1
 
     # Check if the receiver received the message
     grep -q "Subscrib" "$tempdir/recv.log"
@@ -36,9 +121,10 @@ function run_receiver_sender() {
     [[ "143" == "$sender_return_code" ]]
 
     # Cleanup due to SIGINT
-    rm -rf /tmp/mw_com_lola
-    rm -rf /dev/shm/lola-*
+    cleanup_lola
 }
+
+setup
 
 echo -e "\n\n\nRunning Rust receiver and Rust sender"
 run_receiver_sender "$RUST_EXAMPLE_CMD" "$RUST_EXAMPLE_CMD"
