2ab01c08e17b6db7729eaafad54e17934fe1f1fb
Jan Gueth <jan.gueth@etas.com>
2026-01-14 15:36:59 +0100

    Initialize docs-as-code for communication repo
    Add initial Sphinx docs and integrate docs-as-code tooling

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+# *******************************************************************************
+# Copyright (c) 2026 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+# *******************************************************************************
+
+project = "Score Communication"
+project_url = "https://eclipse-score.github.io/score-communication/"
+version = "0.1"
+
+extensions = [
+    # TODO remove plantuml here once docs-as-code is updated to sphinx-needs 6
+    "sphinxcontrib.plantuml",
+    "score_sphinx_bundle",
+]
