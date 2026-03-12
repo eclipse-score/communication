6b69fb6b1acbe72f09eeb4374d3063f7eee1f634
Brendan Emery <brendan.be.emery@bmw.de>
2026-03-12 18:48:27 +0100

    lola: Add Service/Event/EventData Control Local structs
    This commit splits up ServiceDataControl, EventDataControl and
    EventControl into a data class (ServiceDataControl, EventDataControl and
    EventControl) which contains the data that is placed in shared memory
    and behaviour "view" classes (ServiceDataControlLocalView,
    EventDataControlLocalView and EventControlLocalView) which contain behaviour
    for interacting with the data via process local raw pointers which avoid
    performance overhead of interacting with data directly in shared memory
    (due to performance overhead of dealing with OffsetPtrs).

--- score/mw/com/impl/bindings/lola/proxy_event_control_local_view.cpp
+++ score/mw/com/impl/bindings/lola/proxy_event_control_local_view.cpp
@@ -10,4 +10,15 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
-#include "score/mw/com/impl/bindings/lola/event_data_control.h"
+#include "score/mw/com/impl/bindings/lola/proxy_event_control_local_view.h"
+
+namespace score::mw::com::impl::lola
+{
+
+ProxyEventControlLocalView::ProxyEventControlLocalView(EventControl& event_control_shared_mem) noexcept
+    : data_control{event_control_shared_mem.data_control},
+      subscription_control{event_control_shared_mem.subscription_control}
+{
+}
+
+}  // namespace score::mw::com::impl::lola
