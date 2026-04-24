dbee6fa31365bc73602cabc13c703691a94fc285
Krishna <deshkulkarnik@gmail.com>
2026-04-24 18:14:54 +0200

    lola: Extend ProxyMethodBindingFactory field Get/Set tests with Attorney

--- score/mw/com/impl/plumbing/proxy_method_binding_factory_test.cpp
+++ score/mw/com/impl/plumbing/proxy_method_binding_factory_test.cpp
@@ -11,6 +11,8 @@
  * SPDX-License-Identifier: Apache-2.0
  ********************************************************************************/
 #include "score/mw/com/impl/plumbing/proxy_method_binding_factory.h"
+#include "score/mw/com/impl/bindings/lola/methods/proxy_method_instance_identifier.h"
+#include "score/mw/com/impl/bindings/lola/proxy_method.h"
 #include "score/mw/com/impl/bindings/lola/test/proxy_event_test_resources.h"
 #include "score/mw/com/impl/configuration/lola_service_instance_deployment.h"
 #include "score/mw/com/impl/configuration/lola_service_instance_id.h"
@@ -33,6 +35,23 @@
 #include <memory>
 #include <optional>
 #include <utility>
+
+namespace score::mw::com::impl::lola
+{
+class ProxyMethodAttorney
+{
+  public:
+    explicit ProxyMethodAttorney(ProxyMethod& method) noexcept : method_{method} {}
+
+    const ProxyMethodInstanceIdentifier& GetProxyMethodInstanceIdentifier() const noexcept
+    {
+        return method_.proxy_method_instance_identifier_;
+    }
+
+  private:
+    ProxyMethod& method_;
+};
+}  // namespace score::mw::com::impl::lola
 
 namespace score::mw::com::impl
 {
@@ -65,6 +84,28 @@ ConfigurationStore kConfigStoreAsilB{kInstanceSpecifier,
                                      kLolaServiceTypeDeployment,
                                      kLolaServiceInstanceDeployment};
 
+// Deployment with a field alongside the method, for the kGet / kSet tests.
+constexpr auto kDummyFieldName{"Field1"};
+constexpr std::uint16_t kDummyFieldId{6U};
+const auto kFieldInstanceSpecifier = InstanceSpecifier::Create(std::string{"/my_field_instance_specifier"}).value();
+
+const LolaServiceInstanceDeployment kLolaServiceInstanceDeploymentWithField{
+    LolaServiceInstanceId{kInstanceId},
+    {},
+    {{kDummyFieldName, LolaFieldInstanceDeployment{{1U}, {3U}, 1U, true, 0}}},
+    {{kDummyMethodName, LolaMethodInstanceDeployment{kQueueSize}}}};
+
+const LolaServiceTypeDeployment kLolaServiceTypeDeploymentWithField{kServiceId,
+                                                                    {},
+                                                                    {{kDummyFieldName, kDummyFieldId}},
+                                                                    {{kDummyMethodName, kDummyMethodId}}};
+
+ConfigurationStore kConfigStoreWithFieldAsilB{kFieldInstanceSpecifier,
+                                              make_ServiceIdentifierType("/a/service/somewhere/out/there", 13U, 37U),
+                                              kQualityType,
+                                              kLolaServiceTypeDeploymentWithField,
+                                              kLolaServiceInstanceDeploymentWithField};
+
 const LolaServiceInstanceDeployment kLolaServiceInstanceDeploymentWithEmptyQueueSize{
     LolaServiceInstanceId{kInstanceId},
     {},
@@ -85,6 +126,11 @@ class ProxyMethodFactoryFixture : public lola::ProxyMockedMemoryFixture
     HandleType GetValidLoLaHandle()
     {
         return kConfigStoreAsilB.GetHandle();
+    }
+
+    HandleType GetValidLoLaHandleWithField()
+    {
+        return kConfigStoreWithFieldAsilB.GetHandle();
     }
 
     HandleType GetValidSomIpHandle()
@@ -122,7 +168,6 @@ TYPED_TEST_SUITE(ProxyMethodFactoryTypedFixture, RegisteredFunctionTypes, );
 
 TYPED_TEST(ProxyMethodFactoryTypedFixture, CanConstructProxyMethod)
 {
-
     // Given a valid lola binding
 
     const auto handle = this->GetValidLoLaHandle();
@@ -157,7 +202,7 @@ TYPED_TEST(ProxyMethodFactoryTypedFixture, CannotCreateProxyServiceWhenProxyBind
 
 TYPED_TEST(ProxyMethodFactoryTypedFixture, CannotConstructEventFromSomeIpBinding)
 {
-
+    // Given a handle carrying a SomeIp deployment
     const auto handle = this->GetValidSomIpHandle();
 
     // Given a valid someip binding
@@ -174,18 +219,123 @@ TYPED_TEST(ProxyMethodFactoryTypedFixture, CannotConstructEventFromSomeIpBinding
 
 TYPED_TEST(ProxyMethodFactoryTypedFixture, CannotConstructEventFromBlankBinding)
 {
+    // Given a handle carrying a blank deployment
     const auto handle = this->GetBlankBindingHandle();
 
-    // Given a blank binding
+    // When Create is called
     auto proxy_binding = this->CreateBindingFromHandle(handle);
-
-    // When creating a ProxyMethod using MethodBindingFactory
     using MethodSignature = TypeParam;
     auto proxy_method = ProxyMethodBindingFactory<MethodSignature>::Create(
         handle, proxy_binding, kDummyMethodName, MethodType::kMethod);
 
     // Then a nullptr is returned
     EXPECT_EQ(proxy_method, nullptr);
+}
+
+TYPED_TEST(ProxyMethodFactoryTypedFixture, CanConstructFieldGetMethod)
+{
+    // Given a valid lola binding with a field in the deployment
+    const auto handle = this->GetValidLoLaHandleWithField();
+    this->InitialiseProxyWithConstructor(handle.GetInstanceIdentifier());
+
+    // When Create is called with MethodType::kGet
+    auto proxy_binding = this->CreateBindingFromHandle(handle);
+    using MethodSignature = TypeParam;
+    auto proxy_method =
+        ProxyMethodBindingFactory<MethodSignature>::Create(handle, proxy_binding, kDummyFieldName, MethodType::kGet);
+
+    // Then the returned method's id is (field, kGet)
+    ASSERT_NE(proxy_method, nullptr);
+    auto* const lola_method = dynamic_cast<lola::ProxyMethod*>(proxy_method.get());
+    ASSERT_NE(lola_method, nullptr);
+    const auto& unique_id =
+        lola::ProxyMethodAttorney{*lola_method}.GetProxyMethodInstanceIdentifier().unique_method_identifier;
+    EXPECT_EQ(unique_id.method_or_field_id, kDummyFieldId);
+    EXPECT_EQ(unique_id.method_type, MethodType::kGet);
+}
+
+TYPED_TEST(ProxyMethodFactoryTypedFixture, CanConstructFieldSetMethod)
+{
+    // Given a valid lola binding with a field in the deployment
+    const auto handle = this->GetValidLoLaHandleWithField();
+    this->InitialiseProxyWithConstructor(handle.GetInstanceIdentifier());
+
+    // When Create is called with MethodType::kSet
+    auto proxy_binding = this->CreateBindingFromHandle(handle);
+    using MethodSignature = TypeParam;
+    auto proxy_method =
+        ProxyMethodBindingFactory<MethodSignature>::Create(handle, proxy_binding, kDummyFieldName, MethodType::kSet);
+
+    // Then the returned method's id is (field, kSet)
+    ASSERT_NE(proxy_method, nullptr);
+    auto* const lola_method = dynamic_cast<lola::ProxyMethod*>(proxy_method.get());
+    ASSERT_NE(lola_method, nullptr);
+    const auto& unique_id =
+        lola::ProxyMethodAttorney{*lola_method}.GetProxyMethodInstanceIdentifier().unique_method_identifier;
+    EXPECT_EQ(unique_id.method_or_field_id, kDummyFieldId);
+    EXPECT_EQ(unique_id.method_type, MethodType::kSet);
+}
+
+TYPED_TEST(ProxyMethodFactoryTypedFixture, GetAndSetForSameFieldProduceDistinctUniqueIdentifiers)
+{
+    // Given a valid lola binding with a field in the deployment
+    const auto handle = this->GetValidLoLaHandleWithField();
+    this->InitialiseProxyWithConstructor(handle.GetInstanceIdentifier());
+    auto proxy_binding = this->CreateBindingFromHandle(handle);
+    using MethodSignature = TypeParam;
+
+    // When Create is called for the same field name with kGet and then kSet
+    auto get_method =
+        ProxyMethodBindingFactory<MethodSignature>::Create(handle, proxy_binding, kDummyFieldName, MethodType::kGet);
+    auto set_method =
+        ProxyMethodBindingFactory<MethodSignature>::Create(handle, proxy_binding, kDummyFieldName, MethodType::kSet);
+
+    // Then both succeed, share the field id, and differ only on MethodType
+    ASSERT_NE(get_method, nullptr);
+    ASSERT_NE(set_method, nullptr);
+    auto* const lola_get = dynamic_cast<lola::ProxyMethod*>(get_method.get());
+    auto* const lola_set = dynamic_cast<lola::ProxyMethod*>(set_method.get());
+    ASSERT_NE(lola_get, nullptr);
+    ASSERT_NE(lola_set, nullptr);
+
+    const auto& get_id =
+        lola::ProxyMethodAttorney{*lola_get}.GetProxyMethodInstanceIdentifier().unique_method_identifier;
+    const auto& set_id =
+        lola::ProxyMethodAttorney{*lola_set}.GetProxyMethodInstanceIdentifier().unique_method_identifier;
+    EXPECT_EQ(get_id.method_or_field_id, set_id.method_or_field_id);
+    EXPECT_NE(get_id.method_type, set_id.method_type);
+    EXPECT_EQ(get_id.method_type, MethodType::kGet);
+    EXPECT_EQ(set_id.method_type, MethodType::kSet);
+}
+
+TYPED_TEST(ProxyMethodFactoryTypedFixture, CannotConstructFieldGetMethodWhenProxyBindingIsNullptr)
+{
+    // Given a valid lola handle with a field in the deployment
+    const auto handle = this->GetValidLoLaHandleWithField();
+
+    // When Create is called with MethodType::kGet and a null parent binding
+    ProxyBinding* proxy_binding = nullptr;
+    using MethodSignature = TypeParam;
+    auto proxy_method =
+        ProxyMethodBindingFactory<MethodSignature>::Create(handle, proxy_binding, kDummyFieldName, MethodType::kGet);
+
+    // Then nullptr is returned
+    ASSERT_EQ(proxy_method, nullptr);
+}
+
+TYPED_TEST(ProxyMethodFactoryTypedFixture, CannotConstructFieldSetMethodWhenProxyBindingIsNullptr)
+{
+    // Given a valid lola handle with a field in the deployment
+    const auto handle = this->GetValidLoLaHandleWithField();
+
+    // When Create is called with MethodType::kSet and a null parent binding
+    ProxyBinding* proxy_binding = nullptr;
+    using MethodSignature = TypeParam;
+    auto proxy_method =
+        ProxyMethodBindingFactory<MethodSignature>::Create(handle, proxy_binding, kDummyFieldName, MethodType::kSet);
+
+    // Then nullptr is returned
+    ASSERT_EQ(proxy_method, nullptr);
 }
 
 TYPED_TEST(ProxyMethodFactoryTypedFixture, GetQueueSizeReturnsValueForMethodInLolaDeployment)
