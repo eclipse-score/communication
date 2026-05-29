5b747891cb188af9c1a0dd4fb9d64e4bacd45bf2
Krishna Satish Deshkulkarni Shankaranarayana <krishna-satish.deshkulkarni-shankaranarayana@bmw.de>
2026-05-29 20:25:33 +0200

    mw/com: add field_tags.h with WithGetter/WithSetter/WithNotifier definitions
    introduce field_tags.h with tag structs (WithGetter, WithSetter,
    WithNotifier) and the contains_type / is_tag_enabled SFINAE utilities.

---
+++ score/mw/com/impl/field_tags.h
@@ -0,0 +1,69 @@
+/********************************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#ifndef SCORE_MW_COM_IMPL_FIELD_TAGS_H
+#define SCORE_MW_COM_IMPL_FIELD_TAGS_H
+
+#include <type_traits>
+
+namespace score::mw::com::impl
+{
+
+/// \brief Tag types used on ProxyField/SkeletonField level to accomplish SFINAE gating of various signatures,
+/// which depend on the availability of Get/Set/Notifier.
+struct WithGetter
+{
+};
+
+struct WithSetter
+{
+};
+
+struct WithNotifier
+{
+};
+
+template <typename TargetType, typename... Tags>
+struct contains_type : std::disjunction<std::is_same<TargetType, Tags>...>
+{
+};
+
+/// \brief Gates a member function on whether a capability tag was declared for this field.
+///
+/// Two things have to be true for the overload to work:
+///   - \c RequiredTag is somewhere(order does not matter) in \c EnabledTags.
+///   - \c FieldType == \c ClassFieldType (see below)
+///
+/// The second condition (\c FieldType == \c ClassFieldType) is a SFINAE workaround: \c FieldType
+/// must be a deduced local parameter of the member function template (defaulting to \c ClassFieldType)
+/// so the check stays dependent and the compiler does not harderr at class instantiation time.
+///
+/// In practice you use it like this inside \c Foo<FieldType, Tags...>:
+/// \code
+///   template <typename F = FieldType,
+///             std::enable_if_t<is_tag_enabled<F, FieldType, WithSetter, Tags...>::value, int> = 0>
+///   void Set(F value);
+/// \endcode
+///
+/// \tparam FieldType       Local deduced copy of the field type, must default to \c ClassFieldType.
+/// \tparam ClassFieldType  The actual field type the enclosing class was instantiated with.
+/// \tparam RequiredTag     The tag that must be present (e.g. \c WithSetter, \c WithGetter).
+/// \tparam EnabledTags     All tags the field was declared with.
+template <typename FieldType, typename ClassFieldType, typename RequiredTag, typename... EnabledTags>
+struct is_tag_enabled
+    : std::conjunction<contains_type<RequiredTag, EnabledTags...>, std::is_same<FieldType, ClassFieldType>>
+{
+};
+
+}  // namespace score::mw::com::impl
+
+#endif  // SCORE_MW_COM_IMPL_FIELD_TAGS_H
