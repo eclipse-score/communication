8e3f3b218c1c9083627ed1de654c715f49909167
Krishna Satish Deshkulkarni Shankaranarayana <krishna-satish.deshkulkarni-shankaranarayana@bmw.de>
2026-05-31 15:50:42 +0200

    mw/com: migrate ProxyField and SkeletonField to tag-based dispatch
    Replace the fixed bool EnableSet/EnableNotifier template parameters
    with a variadic Tags... pack. WithGetter, WithSetter, WithNotifier
    tag structs control which API is enabled/disabled via SFINAE.

--- score/mw/com/impl/proxy_field_base.h
+++ score/mw/com/impl/proxy_field_base.h
@@ -27,8 +27,16 @@
 namespace score::mw::com::impl
 {
 
+class ProxyBase;
+class ProxyFieldBaseView;
+
 class ProxyFieldBase
 {
+    // Suppress "AUTOSAR C++14 A11-3-1", The rule states: "Friend declarations shall not be used".
+    // Design decision. This class provides a view to the private members of this class.
+    // coverity[autosar_cpp14_a11_3_1_violation]
+    friend class ProxyFieldBaseView;
+
   public:
     ProxyFieldBase(ProxyBase& proxy_base, std::string_view field_name, ProxyEventBase* proxy_event_base_dispatch)
         : proxy_base_{proxy_base}, proxy_event_base_dispatch_{proxy_event_base_dispatch}, field_name_{field_name}
@@ -51,122 +59,77 @@ class ProxyFieldBase
         proxy_base_ = proxy_base;
     }
 
-    /**
-     * \api
-     * \brief Subscribe to the field.
-     * \param max_sample_count Specify the maximum number of concurrent samples that this event shall
-     *                          be able to offer to the using application.
-     * \return On failure, returns an error code.
-     */
+  protected:
     Result<void> Subscribe(const std::size_t max_sample_count) noexcept
     {
         return proxy_event_base_dispatch_->Subscribe(max_sample_count);
     }
 
-    /**
-     * \api
-     * \brief Get the subscription state of this field.
-     * \details This method can always be called regardless of the state of the field.
-     * \return Subscription state of the field.
-     */
     SubscriptionState GetSubscriptionState() noexcept
     {
         return proxy_event_base_dispatch_->GetSubscriptionState();
     }
 
-    /**
-     * \api
-     * \brief End subscription to a field and release needed resources.
-     * \details It is illegal to call this method while data is still held by the application in the form of SamplePtr.
-     *          Doing so will result in undefined behavior. After a call to this method, the field behaves as if it had
-     *          just been constructed.
-     */
-    void Unsubscribe() noexcept
-    {
-        proxy_event_base_dispatch_->Unsubscribe();
-    }
-
-    /**
-     * \api
-     * \brief Sets/Registers a SubscriptionStateChangeHandler for this event. This handler will be called whenever the
-     * subscription state of this event changes.
-     * \note An already set/registered SubscriptionStateChangeHandler will be silently overridden.
-     * \param handler
-     */
     Result<void> SetSubscriptionStateChangeHandler(SubscriptionStateChangeHandler handler) noexcept
     {
         return proxy_event_base_dispatch_->SetSubscriptionStateChangeHandler(std::move(handler));
     }
 
-    /**
-     * \api
-     * \brief Unsets/Unregisters a SubscriptionStateChangeHandler for this field. After this method returns, it is
-     *        guaranteed, that the previously registered handler is neither active nor will be called anymore.
-     */
     Result<void> UnsetSubscriptionStateChangeHandler() noexcept
     {
         return proxy_event_base_dispatch_->UnsetSubscriptionStateChangeHandler();
     }
 
-    /**
-     * \api
-     * \brief Get the number of samples that can still be received by the user of this field.
-     * \details If this returns 0, the user first has to drop at least one SamplePtr before it is possible to receive
-     *          data via GetNewSamples again. If there is no subscription for this field, the returned value is
-     *          unspecified.
-     * \return Number of samples that can still be received.
-     */
     std::size_t GetFreeSampleCount() noexcept
     {
         return proxy_event_base_dispatch_->GetFreeSampleCount();
     }
 
-    /**
-     * \api
-     * \brief Returns the number of new samples a call to GetNewSamples() (given parameter max_num_samples
-     *        doesn't restrict it) would currently provide.
-     * \details This is a proprietary extension to the official ara::com API. It is useful in resource sensitive
-     *          setups, where the user wants to work in polling mode only without registered async receive-handlers.
-     *          For further details see //score/mw/com/design/extensions/README.md.
-     * \return Either 0 if no new samples are available (and GetNewSamples() wouldn't return any) or N, where 1 <= N <=
-     *         actual new samples. I.e. an implementation is allowed to report a lower number than actual new samples,
-     *         which would be provided by a call to GetNewSamples().
-     */
     Result<std::size_t> GetNumNewSamplesAvailable() noexcept
     {
         return proxy_event_base_dispatch_->GetNumNewSamplesAvailable();
     }
 
-    /**
-     * \api
-     * \brief Sets the handler to be called, whenever a new field value has been received.
-     * \details Generally a ReceiveHandler has no restrictions on what mw::com API it is allowed to call.
-     *          It is especially allowed to call all public APIs of the Field instance on which it had been
-     *          set/registered as long as it obeys the general requirement, that API calls on a Proxy/Proxy field are
-     *          thread safe/can't be called concurrently.
-     * \attention This function MUST NOT be called from the context of a ReceiveHandler registered for this field!
-     *            It makes semantically not really sense to register a "new" ReceiveHandler from the context of an
-     *            already running ReceiveHandler. We also see no use cases for it and won't support it therefore.
-     * \param handler user provided handler to be called
-     */
     Result<void> SetReceiveHandler(EventReceiveHandler handler) noexcept
     {
         return proxy_event_base_dispatch_->SetReceiveHandler(std::move(handler));
     }
 
-    /**
-     * \api
-     * \brief Removes any ReceiveHandler registered via SetReceiveHandler.
-     */
     Result<void> UnsetReceiveHandler() noexcept
     {
         return proxy_event_base_dispatch_->UnsetReceiveHandler();
     }
 
-  protected:
+    void Unsubscribe() noexcept
+    {
+        // if the WithNotifier tag is not set, proxy_event_base_dispatch_ will be nullptr.
+        if (proxy_event_base_dispatch_ != nullptr)
+        {
+            proxy_event_base_dispatch_->Unsubscribe();
+        }
+    }
+
     std::reference_wrapper<ProxyBase> proxy_base_;
     ProxyEventBase* proxy_event_base_dispatch_;
     std::string_view field_name_;
+};
+
+/// \brief View pattern to access ProxyFieldBase's private members and functions from ProxyBase without making them
+/// public.
+///
+/// A method that must be visible to the impl namespace but hidden from the end user.
+class ProxyFieldBaseView
+{
+  public:
+    explicit ProxyFieldBaseView(ProxyFieldBase& base) noexcept : base_{base} {}
+
+    void Unsubscribe() noexcept
+    {
+        base_.Unsubscribe();
+    }
+
+  private:
+    ProxyFieldBase& base_;
 };
 
 }  // namespace score::mw::com::impl
