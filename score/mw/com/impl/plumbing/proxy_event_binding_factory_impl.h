5607c41b1817d2556515a298ccb4b299205a5088
Krishna Satish Deshkulkarni Shankaranarayana <krishna-satish.deshkulkarni-shankaranarayana@bmw.de>
2026-06-11 15:12:40 +0200

    mw/com: replace LoLaProxyElementBuildingBlocks with lola lookup helpers
    split LookupLolaProxyElement into two free functions in bindings/lola and rework all the callers

--- score/mw/com/impl/plumbing/proxy_event_binding_factory_impl.h
+++ score/mw/com/impl/plumbing/proxy_event_binding_factory_impl.h
@@ -61,28 +61,28 @@ class GenericProxyEventBindingFactoryImpl : public IGenericProxyEventBindingFact
 };
 
 template <typename SampleType>
-// Suppress "AUTOSAR C++14 A15-5-3" rule finding. This rule states: "The std::terminate() function shall
-// not be called implicitly.". std::visit Throws std::bad_variant_access if
-// as-variant(vars_i).valueless_by_exception() is true for any variant vars_i in vars. The variant may only become
-// valueless if an exception is thrown during different stages. Since we don't throw exceptions, it's not possible
-// that the variant can return true from valueless_by_exception and therefore not possible that std::visit throws
-// an exception.
-// This suppression should be removed after fixing [Ticket-173043](broken_link_j/Ticket-173043)
-// coverity[autosar_cpp14_a15_5_3_violation : FALSE]
 inline std::unique_ptr<ProxyEventBinding<SampleType>> ProxyEventBindingFactoryImpl<SampleType>::Create(
     ProxyBase& parent,
     const std::string_view event_name,
     const ServiceElementType element_type) noexcept
 {
-    const auto lookup = LookupLolaProxyElement(parent, event_name, element_type);
-    if (!lookup.has_value())
+    auto* const lola_proxy = lola::GetLolaProxyBinding(ProxyBaseView{parent}.GetBinding());
+    if (lola_proxy == nullptr)
     {
         score::mw::log::LogError("lola")
             << "ProxyEvent binding could not be created for event" << event_name
             << "because the parent proxy binding is not a lola binding or the element could not be resolved.";
         return nullptr;
     }
-    return std::make_unique<lola::ProxyEvent<SampleType>>(lookup->parent, lookup->element_fq_id, event_name);
+    const auto element_fq_id = lola::GetElementFqId(parent.GetHandle(), event_name, element_type);
+    if (!element_fq_id.has_value())
+    {
+        score::mw::log::LogError("lola")
+            << "ProxyEvent binding could not be created for event" << event_name
+            << "because the parent proxy binding is not a lola binding or the element could not be resolved.";
+        return nullptr;
+    }
+    return std::make_unique<lola::ProxyEvent<SampleType>>(*lola_proxy, *element_fq_id, event_name);
 }
 
 }  // namespace score::mw::com::impl
