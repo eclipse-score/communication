ae7a3a689d613a0c540eb3592086bd3e27418419
Krishna Satish Deshkulkarni Shankaranarayana <krishna-satish.deshkulkarni-shankaranarayana@bmw.de>
2026-06-11 15:12:40 +0200

    mw/com: replace LoLaProxyElementBuildingBlocks with lola lookup helpers
    split LookupLolaProxyElement into two free functions in bindings/lola and rework all the callers

--- score/mw/com/impl/plumbing/proxy_method_binding_factory_impl.h
+++ score/mw/com/impl/plumbing/proxy_method_binding_factory_impl.h
@@ -105,8 +105,16 @@ std::unique_ptr<ProxyMethodBinding> ProxyMethodBindingFactoryImpl<ReturnType(Arg
                                   ? ServiceElementType::FIELD
                                   : ServiceElementType::METHOD;
 
-    const auto lookup = LookupLolaProxyElement(parent_handle, parent_binding, method_name, element_type);
-    if (!lookup.has_value())
+    auto* const lola_proxy = lola::GetLolaProxyBinding(parent_binding);
+    if (lola_proxy == nullptr)
+    {
+        score::mw::log::LogError("lola")
+            << "Proxy Method binding could not be created for" << method_name
+            << "because the parent proxy binding is not a lola binding or the element could not be resolved.";
+        return nullptr;
+    }
+    const auto element_fq_id = lola::GetElementFqId(parent_handle, method_name, element_type);
+    if (!element_fq_id.has_value())
     {
         score::mw::log::LogError("lola")
             << "Proxy Method binding could not be created for" << method_name
@@ -122,10 +130,9 @@ std::unique_ptr<ProxyMethodBinding> ProxyMethodBindingFactoryImpl<ReturnType(Arg
     // the same id, but the pair (id, method_type) is different, so the two end up in separate
     // entries of the proxy_methods_ / skeleton_methods_ maps in the binding layer.
     const lola::ProxyMethodInstanceIdentifier proxy_method_instance_identifier{
-        lookup->parent.GetProxyInstanceIdentifier(), {lookup->element_fq_id.element_id_, method_type}};
+        lola_proxy->GetProxyInstanceIdentifier(), {element_fq_id->element_id_, method_type}};
 
-    return std::make_unique<lola::ProxyMethod>(
-        lookup->parent, proxy_method_instance_identifier, type_erased_element_info);
+    return std::make_unique<lola::ProxyMethod>(*lola_proxy, proxy_method_instance_identifier, type_erased_element_info);
 }
 
 }  // namespace score::mw::com::impl
