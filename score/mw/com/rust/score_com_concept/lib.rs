0920265ef0f14a19cedaf7ca8c596b01b2815973
bharatgoswami <Bharat.Goswami@bti.bmwgroup.com>
2026-07-31 15:09:30 +0530

    Rust::com Update the Interface macro modules

--- score/mw/com/rust/score_com_concept/lib.rs
+++ score/mw/com/rust/score_com_concept/lib.rs
@@ -24,6 +24,7 @@ mod concept;
 mod error;
 mod field_concept;
 mod interface_consumer_macros;
+mod interface_macros;
 mod interface_producer_macros;
 mod method_arities_macros;
 mod method_concept;
@@ -31,7 +32,9 @@ mod reloc;
 pub use concept::*;
 pub use error::*;
 pub use field_concept::*;
-pub use interface_consumer_macros::{HandlerNotSet, HandlerSet, Init, Uninit, WithGetter, WithNotifier, WithSetter};
+pub use interface_macros::{
+    HandlerNotSet, HandlerSet, Init, Uninit, WithGetter, WithNotifier, WithSetter,
+};
 pub use method_concept::*;
 #[doc(hidden)]
 pub use paste;
