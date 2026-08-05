65eaa1640ccf0b910fd0a6925f22e98b819bda97
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2026-08-05 15:55:51 +0000

    remove unused function

--- score/socom/payload.hpp
+++ score/socom/payload.hpp
@@ -102,8 +102,6 @@ class Payload_impl final {
 
     bool operator==(Payload_impl const& other) const noexcept;
 
-    bool operator!=(Payload_impl const& other) const noexcept { return !(*this == other); }
-
    private:
     void call_payload_destroyed() noexcept {
         if (!m_payload_destroyed.empty()) {
