4a594a5facac11705afe8565dbe3f4041b67610a
George Dadunashvili <george.dadunashvili@bmw.de>
2026-09-02 17:19:01 +0200

    mw/com/dependability: link ProxyFieldGetSubscriptionState

--- score/mw/com/impl/proxy_event_base_test.cpp
+++ score/mw/com/impl/proxy_event_base_test.cpp
@@ -845,7 +845,8 @@ TYPED_TEST(ProxyEventBaseSubscribeFixture,
 
 TYPED_TEST(ProxyEventBaseGetSubscriptionStatefixture, GetSubscriptionStateDispatchesToBinding)
 {
-    this->RecordProperty("Verifies", "SCR-14034825, SCR-17292400");
+    this->RecordProperty("Verifies", "SCR-14034825");
+    this->RecordProperty("lobster-tracing", "Communication.ProxyFieldGetSubscriptionState");
     this->RecordProperty("lobster-tracing", "Communication.ProxyEventGetSubscriptionState");
     this->RecordProperty("Description",
                          "Checks that GetSubscriptionState will return the free sample count returned by the binding");
